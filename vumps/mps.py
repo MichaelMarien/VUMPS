fdd094a8de663c7163df4dcbfb9dd6ddb98be3be
michaelmarien <marien.mich@gmail.com>
2019-08-17 20:42:25 +0200

    added checks and internal properties of dimensions

--- vumps/mps.py
+++ vumps/mps.py
@@ -1,4 +1,5 @@
 from tensornetwork import TensorNetwork
+import numpy as np
 
 
 class MPS(TensorNetwork):
@@ -8,13 +9,19 @@ class MPS(TensorNetwork):
     def __init__(self, tensor, left_virtual=0, physical=1, right_virtual=2,
                  backend="numpy"):
         super().__init__(backend)
-
+        if tensor.shape[left_virtual] != tensor.shape[right_virtual]:
+            raise ValueError("Left and right virtual dimension must be equal")
         # Create the network
         tensor = self.backend.transpose(tensor,
                                         [left_virtual, physical, right_virtual])
         self.A = self.add_node(tensor, axis_names=self.axis_order)
+        self.virtual_dimension = self.backend(self.A.tensor.shape)[0]
+        self.physical_dimension = self.backend(self.A.tensor.shape)[1]
 
-    def _QRPos(self, L, A):
+    def _QRPos(self, L):
+        #LA = np.dot(L, self.backend.reshape(self.A.tensor,
+        #                                    (self.backend.shape[0])))
+        #np.linalg.qr()
         raise NotImplementedError
 
     def left_orthonormalize(self, initial, epsilon):
