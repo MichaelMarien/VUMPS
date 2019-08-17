51d41b233fbd4e59cada634629dff0186a7a4b55
michaelmarien <marien.mich@gmail.com>
2019-08-17 20:27:39 +0200

    order left-physical-right

--- vumps/mps.py
+++ vumps/mps.py
@@ -3,17 +3,16 @@ from tensornetwork import TensorNetwork
 
 class MPS(TensorNetwork):
 
+    axis_order = ["left_virtual", "physical", "right_virtual"]
+
     def __init__(self, tensor, left_virtual=0, physical=1, right_virtual=2,
                  backend="numpy"):
         super().__init__(backend)
 
         # Create the network
-        axis_dict = {"left_virtual": left_virtual,
-                     "right_virtual": right_virtual,
-                     "physical": physical}
-
-        self.axis_ordering = sorted(axis_dict, key=axis_dict.get, reverse=False)
-        self.A = self.add_node(tensor, axis_names=self.axis_ordering)
+        tensor = self.backend.transpose(tensor,
+                                        [left_virtual, physical, right_virtual])
+        self.A = self.add_node(tensor, axis_names=self.axis_order)
 
     def _QRPos(self, L, A):
         raise NotImplementedError
