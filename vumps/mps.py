96b450c43a6c6ecefc9124821c3d5978b7fc9079
michaelmarien <marien.mich@gmail.com>
2019-08-17 20:56:16 +0200

    fixed bug in code calling shape

--- vumps/mps.py
+++ vumps/mps.py
@@ -15,14 +15,17 @@ class MPS(TensorNetwork):
         tensor = self.backend.transpose(tensor,
                                         [left_virtual, physical, right_virtual])
         self.A = self.add_node(tensor, axis_names=self.axis_order)
-        self._virtual_dimension = self.backend(self.A.tensor.shape)[0]
-        self._physical_dimension = self.backend(self.A.tensor.shape)[1]
+        self._virtual_dimension = self.backend.shape_tuple(self.A.tensor)[0]
+        self._physical_dimension = self.backend.shape_tuple(self.A.tensor)[1]
 
     def _QRPos(self, L):
-        #LA = np.dot(L, self.backend.reshape(self.A.tensor,
-        #                                    (self.backend.shape[0])))
-        #np.linalg.qr()
-        raise NotImplementedError
+        new_shape = (self.virtual_dimension*self.physical_dimension,
+                     self.virtual_dimension)
+        la = np.dot(L, self.backend.reshape(self.A.tensor, new_shape))
+        a_new, l_new = np.linalg.qr(la, mode="reduced")
+        phases = 1j*np.angle(np.diag(a_new))
+        return (np.dot(a_new, np.diag(np.exp(phases))),
+                np.dot(l_new, np.diag(np.exp(-phases))))
 
     def left_orthonormalize(self, initial, epsilon):
         raise NotImplementedError
